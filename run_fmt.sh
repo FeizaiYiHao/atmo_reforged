05a324860947ca98fc2d424949cf1c6b30c0b483
FeizaiYiHao <xiangdc@bu.edu>
2024-11-25 16:09:31 -0700

    add script for running verus fmt

---
+++ run_fmt.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+verusfmt ./util/*
+verusfmt ./kernel/*
+verusfmt ./allocator/*
+verusfmt ./slinkedlist/*
+verusfmt ./process_manager/*
+verusfmt ./memory_manager/*
+verusfmt ./pagetable/*
+verusfmt ./lemma/*
+verusfmt ./trap.rs
+verusfmt ./va_range.rs
+verusfmt ./define.rs
